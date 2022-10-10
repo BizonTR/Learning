c8820d79ad4a26e2e39401c2a67aa9e953846e32
Bizon TR <bizongames@hotmail.com.tr>
2022-10-10 14:07:23 +0300

    pushed

--- ClassLibrary1/Employee.cs
+++ ClassLibrary1/Employee.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +10,48 @@ namespace ClassLibrary1
 {
     public class Employee
     {
-        public string Name { get; set; }
+        public string Name { get; set; } //get varsa bu değişken farklı metodlar tarafından okunabilir, set varsa bu değişkene bir değer yazılabilir.
         public string LastName { get; set; }
         public string EmployeeId { get; set; }
-        public double Salary { get; set; }
         public string Id { get; set; }
         public string Office { get; set; }
+
+        //propfull tab tab
+        private string _freeinfo;
+
+        public string FreeInfo
+        {
+            get { return _freeinfo.ToUpper(); }
+            set { _freeinfo = value; }
+        }
+
+        private int _salary;
+
+        public int Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value < 5500)
+                {
+                    value = 5500;
+                }
+                else
+                {
+                    value = value;
+                }
+                _salary= value;
+            }
+            
+        }
+
+        public string FullName { 
+            get
+            {
+                return string.Join(Name," ",LastName);
+            }
+            }
+
+
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,9 +2,14 @@
 
 Employee emp1 = new Employee();
 
-emp1.Name = "Batu";
-emp1.LastName = "Sutcu";
+emp1.Name = "Ahmet";
+emp1.LastName = "Can";
 emp1.Salary = 10000;
 
-Console.WriteLine(emp1.Name);
+Console.WriteLine(emp1.FullName);
+Console.WriteLine(emp1.Salary);
+
+Employee emp2 = new Employee();
+emp2.FreeInfo = "Lorem Ipsum";
+Console.WriteLine(emp2.FreeInfo);
 
